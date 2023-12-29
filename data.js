5088c2ed47038bfc62d56c439cbf134915809514
Tabarez Pablo <101540610+PabloTabarez@users.noreply.github.com>
2023-12-29 23:23:39 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const libros = [
+        { id: '1', titulo: 'El Arte de la Guerra', autor: 'Sun Tzu' },
+        { id: '2', titulo: 'El principe', autor: 'Nicolas Maquiavelo' },
+        { id: '3', titulo: 'La Hojarasca', autor: 'Gabriel García Márquez' }
+    ];
+module.exports = libros;
